e31c35356862746c7fb90a0c3cc4006033ffac11
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-04-09 08:55:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary min-h-screen flex flex-col justify-between">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {  
+    const [isActive, setActive] = useState("false");
+
+    // Click Humberger Handler
+    const humbergerClick = (e) => {
+        // hamburger.classList.toggle('hamburger-active');
+        // navMenu.classList.toggle('hidden');
+        setActive(!isActive); 
+    }
+
+    return ( 
+        <div className="top-0 left-0 w-full">
+            <div className="md:flex bg-white md:p-4 justify-between">
+                <div className="md:px-6 p-3 md:p-0 text-black flex items-center mb-3 justify-between md:justify-start">
+                    <p className="font-bold text-2xl ml-2 md:ml-0">Bonevian</p>
+
+                    <button id="hamburger" name="hamburger" onClick={humbergerClick} type="button" className={isActive ? "null md:hidden mr-2 md:mr-0" : "hamburger-active md:hidden mr-2 md:mr-0"}>
+                        <span className="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
+                        <span className="hamburger-line transition duration-300 ease-in-out"></span>
+                        <span className="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+                    </button>
+                </div>
+                <ul id="nav-menu" onClick={humbergerClick} className={isActive ? "hidden md:flex flex-col md:flex-row items-center" : "null flex flex-col md:flex-row items-center animate-wiggle"}>
+                    <li className="md:ml-4 w-full text-center border-t-2 border-black p-1 md:border-none md:p-0"><NavLink to="/" className="nav-item">Home</NavLink></li>
+                    <li className="md:ml-4 w-full text-center border-t-2 border-black p-1 md:border-none md:p-0"><NavLink to="/about" className="nav-item">About</NavLink></li>
+                    <li className="md:ml-4 w-full text-center border-t-2 border-black p-1 md:border-none md:p-0"><NavLink to="/projects" className="nav-item">Projects</NavLink></li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
